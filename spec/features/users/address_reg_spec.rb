f84c540d7386dc6cfc05cb9bc527b64073add207
Tay <tay.james.d@gmail.com>
2019-07-26 21:52:02 -0600

    Updates testing/models/views to account for deleted attributes on Users table

---
+++ spec/features/users/address_reg_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe "User registration with address" do
+  xit "can register a new user" do
+    visit root_path
+
+    click_on "Register"
+    save_and_open_page
+    # fill_in :
+  end
+end

c92dc41d8ef7c2bb6334ac0a44c664ecc53388b8
Tay <tay.james.d@gmail.com>
2019-07-29 23:56:42 -0600

    Tests that a user can properly delete their address

---
+++ spec/features/user/address/delete_spec.rb
@@ -0,0 +1,42 @@
+require "rails_helper"
+
+RSpec.describe 'Delete Address' do
+  describe 'As a registered user' do
+    before :each do
+      @user = User.create!(name: 'Megan', email: 'megan@example.com', password: 'securepassword')
+      @home = @user.addresses.create!(street: "1111 Ash St.", city: "Denver", state: "CO", zip: "80220")
+      @casita = @user.addresses.create!(street: "2 LaHabre Dr.", city: "Pueblo", state: "CO", zip: "81005", nickname: "casita")
+      # binding.pry
+    end
+
+    it "I can click a link to delete that address" do
+      visit login_path
+
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      click_button 'Log In'
+
+      within "#address-#{@home.id}" do
+        click_button 'Delete'
+      end
+      #put info before user hits delete
+      expect(current_path).to eq(profile_path)
+      expect(page).to_not have_content(@home.id)
+    end
+
+    it "I cannot delete an address if it's being used for a shipped order" do
+      order = Order.create!(user: @user, address_id: @casita.id, status: 'shipped')
+
+      visit login_path
+
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      click_button 'Log In'
+      # binding.pry
+      # save_and_open_page
+      within "#address-#{@casita.id}" do
+        expect(page).to_not have_button("Delete Address")
+      end
+    end
+  end
+end

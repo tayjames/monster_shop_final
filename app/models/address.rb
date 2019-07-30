cca1b7d40090e428e2f0e70684834a1c0dc47f33
Tay <tay.james.d@gmail.com>
2019-07-30 00:02:13 -0600

    Adds shipped_orders method to Address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -7,4 +7,8 @@ class Address < ApplicationRecord
   validates_presence_of :city
   validates_presence_of :state
   validates_presence_of :zip
+
+  def shipped_orders
+    orders.where(status: 'shipped')
+  end
 end
